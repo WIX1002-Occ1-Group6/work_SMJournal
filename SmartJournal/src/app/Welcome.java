2149cfd69f07cff29747e9f07dd2fdf5acbdb9c6
sprball <sprball@icloud.com>
2026-01-10 15:40:17 +0800

    fix

--- SmartJournal/src/app/Welcome.java
+++ SmartJournal/src/app/Welcome.java
@@ -23,21 +23,24 @@ public class Welcome {
     }
 
     public boolean displayMainMenu(String displayName, String email) {
+        journal.clearScreen();
         System.out.println("\n" + getGreeting() + ", " + displayName + "!");
-        System.out.println("--- Main Menu ---");
+        System.out.println("=== Main Menu ===");
         System.out.println("1. Create, Edit & View Journals");
         System.out.println("2. View Weekly Mood Summary");
         System.out.println("3. Log out");
-        System.out.println("Please select an option: ");
+        System.out.print("Please select an option:\n> ");
         int choice = scanner.nextInt();
         switch (choice) {
             case 1:
-                journal.datePage(email);
+                boolean exit = false;
+                while (exit == false) exit = journalDatePage(email);
                 break;
             case 2:
                 // TODO
                 break;
             case 3:
+                journal.clearScreen();
                 return false;
             default:
                 break;
@@ -45,4 +48,19 @@ public class Welcome {
         return true;
     }
 
+    private boolean journalDatePage(String email) {
+        journal.clearScreen();
+        int countJournal = journal.datePage(email);
+        int journalDateNum = -1;
+        while (journalDateNum < 0 || journalDateNum > countJournal) {
+            System.out.println("\nSelect a date to view journal, or create a new journal for today:");
+            System.out.print("> ");
+            journalDateNum = scanner.nextInt();
+        }
+        if (journalDateNum > 0) {
+            boolean exit = false;
+            while (exit == false) exit = journal.journalPage(journalDateNum, email);
+        }
+        return journalDateNum == 0;
+    }
 }
